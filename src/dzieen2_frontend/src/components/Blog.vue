c43ae0e1b975777c1a6275cbd7ed621bd5351f91
AdisS2 <aaugustyn222@gmail.com>
2024-06-27 16:58:58 +0200

    added way to list wpisy

--- src/dzieen2_frontend/src/components/Blog.vue
+++ src/dzieen2_frontend/src/components/Blog.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
+        <button @click="pobierzWpisy">refresh</button>
         siema blog!
+        {{ wpisy }}
     </div>
 </template>
+
+<script>
+import { dzieen2_backend } from 'declarations/dzieen2_backend/index';
+
+export default {
+    data () {
+        return {
+            wpisy:[]
+        }
+    },
+    methods: {
+        async pobierzWpisy() {
+            this.wpisy = await dzieen2_backend.odczytaj_wpisy();
+        }
+    },
+}
+</script>
