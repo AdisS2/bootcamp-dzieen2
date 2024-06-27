2015f6f7acd397cff044e7476b7b5633e18ad9c4
AdisS2 <aaugustyn222@gmail.com>
2024-06-27 17:53:48 +0200

    added a way to add new wpis

--- src/dzieen2_frontend/src/components/Blog.vue
+++ src/dzieen2_frontend/src/components/Blog.vue
@@ -3,6 +3,8 @@
         <button @click="pobierzWpisy">refresh</button>
         siema blog!
         {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpisy">dodaj</button>
     </div>
 </template>
 
@@ -10,12 +12,16 @@
 import { dzieen2_backend } from 'declarations/dzieen2_backend/index';
 
 export default {
-    data () {
+    data() {
         return {
-            wpisy:[]
+            wpisy: [],
+            nowyBlog: ""
         }
     },
     methods: {
+        async dodajWpisy() {
+            await dzieen2_backend.dodaj_wpis(this.nowyBlog);
+        },
         async pobierzWpisy() {
             this.wpisy = await dzieen2_backend.odczytaj_wpisy();
         }
